2cf8015c2d1240222de753d3088462c7c0ca0e66
andr777 <andr.root@gmail.com>
2022-09-07 03:57:17 +0300

    вывод в консоль:
    список сотрудников пуст
    добавлен сотрудник
    Id: 0   Ф.И.О.:Ivanov Ivan I   отдел: 1   зарплата: 100.0$
    Сумма зарплат за месяц составила 100.0 $$
    Среднее зарплат за месяц составила 100.0 $$
    Мин зарплата Id:0 Ivanov Ivan зарплата: 100.0$
    Макс зарплата Id:0 Ivanov Ivan зарплата: 100.0$
    добавлен сотрудник
    Id: 0   Ф.И.О.:Ivanov Ivan I   отдел: 1   зарплата: 100.0$
    Id: 1   Ф.И.О.:Petrov Petr I   отдел: 3   зарплата: 150.0$
    Id: 2   Ф.И.О.:fgjdbj tuyi L   отдел: 3   зарплата: 1500.0$
    Id: 3   Ф.И.О.:dukfguk gku H   отдел: 3   зарплата: 250.0$
    Сумма зарплат за месяц составила 2000.0 $$
    Среднее зарплат за месяц составила 500.0 $$
    Мин зарплата Id:0 Ivanov Ivan зарплата: 100.0$
    Макс зарплата Id:2 fgjdbj tuyi зарплата: 1500.0$
    Ivanov Ivan I
    Petrov Petr I
    fgjdbj tuyi L
    dukfguk gku H
    
    Process finished with exit code 0

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,76 @@
 public class Employee {
+    String surname;         // фамилия
+    String name;            // имя
+    String patronymic;      // отчество
+    String department;      // отдел
+    double salary;          // зарплата
+    int id;                 // номер
+
+
+    public Employee(String surname, String name, String patronymic, String department, double salary) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.department = department;
+        this.salary = salary;
+        this.id = Main.idCounter;
+        Main.idCounter++;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+
+
+
+    /* FULL NAME.
+
+     */
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,136 @@
 public class Main {
+
+    static Employee employees[] = new Employee[10];  // массив на 10 сотрудников
+    static int idCounter = 0;   // счётчик номера, счётчик сотрудниеов
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//тестовые данные и проверка
+        listEmployee(); // проверка метода вывод полного списка полей
+       // System.out.println("Сумма зарплат за месяц составила " + monthlySalary() + " $$");
+
+
+        employees[0] = new Employee("Ivanov", "Ivan", "I", "1", 100D);
+        System.out.println("добавлен сотрудник");
+        listEmployee(); // проверка метода вывод полного списка полей
+        System.out.println("Сумма зарплат за месяц составила " + monthlySalary() + " $$");  // проверка метода вывод Сумма зарплат за месяц
+        System.out.println("Среднее зарплат за месяц составила " + salaryAverage() + " $$");  // проверка метода вывод Среднее зарплат за месяц
+        System.out.println("Мин зарплата Id:" + employees[employeesMin()].getId() + " "
+                + employees[employeesMin()].getSurname() + " "
+                + employees[employeesMin()].getName()
+                + " зарплата: " + employees[employeesMin()].getSalary() + "$");           // проверка метода вывод сотрудника с мин зарплат за месяц
+        System.out.println("Макс зарплата Id:" + employees[employeesMax()].getId() + " "
+                + employees[employeesMax()].getSurname() + " "
+                + employees[employeesMax()].getName()
+                + " зарплата: " + employees[employeesMax()].getSalary() + "$");         // проверка метода вывод сотрудника с Max зарплат за месяц
+
+        employees[1] = new Employee("Petrov", "Petr", "I", "3", 150D);
+        employees[2] = new Employee("fgjdbj", "tuyi", "L", "3", 1500D);
+        employees[3] = new Employee("dukfguk", "gku", "H", "3", 250D);
+
+        System.out.println("добавлен сотрудник");
+        listEmployee(); // проверка метода вывод полного списка полей
+        System.out.println("Сумма зарплат за месяц составила " + monthlySalary() + " $$"); // проверка метода вывод Сумма зарплат за месяц
+
+        System.out.println("Среднее зарплат за месяц составила " + salaryAverage() + " $$");  // проверка метода вывод Среднее зарплат за месяц
+
+        System.out.println("Мин зарплата Id:" + employees[employeesMin()].getId() + " "
+                + employees[employeesMin()].getSurname() + " "
+                + employees[employeesMin()].getName()
+                + " зарплата: " + employees[employeesMin()].getSalary() + "$");             // проверка метода вывод сотрудника с мин зарплат за месяц
+        System.out.println("Макс зарплата Id:" + employees[employeesMax()].getId() + " "
+                + employees[employeesMax()].getSurname() + " "
+                + employees[employeesMax()].getName()
+                + " зарплата: " + employees[employeesMax()].getSalary() + "$");            // проверка метода вывод сотрудника с Max зарплат за месяц
+
+        listEmployeeFullName(); // проверка метода вывод списка ФИО
+//тестовые данные и проверка
+
+
+    } // конец метода майн
+
+    public static void listEmployee() {
+        if (idCounter == 0) {
+            System.out.println("список сотрудников пуст");
+            //throw new IllegalArgumentException("список сотрудников пуст. ");
+            return;
+        } else {
+            for (int i = 0; i < idCounter; i++) {
+                System.out.println("Id: " + employees[i].getId() + "   Ф.И.О.:"
+                        + employees[i].getSurname() + " " + employees[i].getName() + " " + employees[i].getPatronymic()
+                        + "   отдел: " + employees[i].getDepartment()
+                        + "   зарплата: " + employees[i].getSalary() + "$");
+            }
+
+        }
+        return;
     }
+
+    public static void listEmployeeFullName() {
+        if (idCounter == 0) {
+            System.out.println("список сотрудников пуст");
+            throw new IllegalArgumentException("список сотрудников пуст. ");
+            //return;
+        } else {
+            for (int i = 0; i < idCounter; i++) {
+                System.out.println(employees[i].getSurname() + " " + employees[i].getName() + " " + employees[i].getPatronymic());
+            }
+
+        }
+        return;
+    }
+
+    public static double monthlySalary() {
+        double monthlySalary = 0;
+        for (int i = 0; i < idCounter; i++) {
+            monthlySalary = monthlySalary + employees[i].getSalary();
+        }
+        return monthlySalary;
+    }
+
+    public static int employeesMin() {
+        if (idCounter == 0) {
+            System.out.println("список сотрудников пуст.");
+            throw new IllegalArgumentException("список сотрудников пуст. ");
+            //return 0;
+        }
+        double summaMin = employees[idCounter - 1].getSalary();
+        int employeesMin = 0;
+        for (int i = 0; i < idCounter; i++) {
+            if (employees[i].getSalary() < summaMin) {
+                summaMin = employees[i].getSalary();
+                employeesMin = i;
+            }
+        }
+        return employeesMin;
+    }
+
+
+    public static int employeesMax() {
+        if (idCounter == 0) {
+            System.out.println("список сотрудников пуст.");
+            throw new IllegalArgumentException("список сотрудников пуст. ");
+            //return 0;
+        }
+        double summaMax = employees[idCounter - 1].getSalary();
+        int employeesMax = idCounter - 1;
+        for (int i = 0; i < idCounter; i++) {
+            if (employees[i].getSalary() > summaMax) {
+                summaMax = employees[i].getSalary();
+                employeesMax = i;
+            }
+        }
+        return employeesMax;
+    }
+
+    public static double salaryAverage() {
+        if (idCounter == 0) {
+            System.out.println("список сотрудников пуст.");
+            throw new IllegalArgumentException("список сотрудников пуст. средняя зарплата отсутствует.");
+            //return 0;
+        }
+        return monthlySalary() / idCounter;
+
+
+    }
+
 }
